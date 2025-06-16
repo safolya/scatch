a55b9c8d13a9adfc682aa721242ca717704ef2bf
Safolya <safolyamondal12345@gmail.com>
2025-06-17 00:12:00 +0530

    debuger

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const config=require("config");
+const dbgr=require("debug")("development:mongoose");
 
 main().then(()=>{
-    console.log("connection success");
+    dbgr("connection success");
 })
-.catch(err => console.log(err));
+.catch(err => dbgr(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/scatch');
+  await mongoose.connect(`${config.get("MONGODB_URI")}/scatch`);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 };
