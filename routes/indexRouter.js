6e67fc51517be4c741705073fb870862fb1166c1
Safolya <safolyamondal12345@gmail.com>
2025-06-23 13:53:16 +0530

    cart

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,15 +4,31 @@ const router=express.Router();
 const isloggedin=require("../middlewares/isLoggedin");
 const productModel=require("../models/products-model");
 const isLoggedin = require("../middlewares/isLoggedin");
+const userModel=require("../models/user-model")
 
 router.get("/",(req,res)=>{
     let error=req.flash("error");
     res.render("index",{error,loggedin:false});
 });
 
+
 router.get("/shop",isloggedin,async(req,res)=>{
     let products=await productModel.find();
-    res.render("shop.ejs",{products});
-})
+    let success=req.flash("success");
+    res.render("shop.ejs",{products,success});
+});
+
+router.get("/cart",isloggedin,async(req,res)=>{
+    let user=await userModel.findOne({email: req.user.email}).populate("cart");
+    res.render("cart.ejs",{user});
+});
+
+router.get("/addtocart/:id",isloggedin,async(req,res)=>{
+    let user=await userModel.findOne({email: req.user.email});
+    user.cart.push(req.params.id);
+    await user.save();
+    req.flash("success","Added to Cart");
+    res.redirect("/shop");
+});
 
 module.exports=router;
